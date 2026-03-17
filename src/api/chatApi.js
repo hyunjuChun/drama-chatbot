63e5a93d9f8f3d904ffb40a437f5b4cb64388642
chj <hyunjuchunn@gmail.com>
2026-03-17 17:54:36 +0900

    api 수정

--- src/api/chatApi.js
+++ src/api/chatApi.js
@@ -1,6 +1,7 @@
 export const sendChatMessage = async (message, characterName, characterDesc) => {
     try {
-        const response = await fetch("http://localhost:3000/api/chat", {
+        // const response = await fetch("http://localhost:3000/api/chat", {
+        const response = await fetch("https://drama-chatbot.onrender.com/api/chat", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ 
